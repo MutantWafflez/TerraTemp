5aa30d8760823a04d77b98dfd5942af6832bf220
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-12-24 23:28:56 -0800

    Worm Scarf Change

---
+++ Content/Changes/ItemChanges/Accessories/WormScarf.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class WormScarf : ItemChange {
+        public override int AppliedItemID => ItemID.WormScarf;
+
+        public override float ColdComfortabilityChange => -2f;
+    }
+}
