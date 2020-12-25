2b5a0a27d48f4037344f23bbc3df0c4d60784bf2
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-12-24 23:20:10 -0800

    Vanilla Cape Changes

---
+++ Content/Changes/ItemChanges/Accessories/Capes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class Capes : ItemChange {
+        public override int AppliedItemID => ItemID.CrimsonCloak;
+
+        public override List<int> AlternativeIDs => new List<int> {
+            ItemID.MysteriousCape,
+            ItemID.RedCape,
+            ItemID.WinterCape
+        };
+
+        public override float ColdComfortabilityChange => -5f;
+    }
+}
