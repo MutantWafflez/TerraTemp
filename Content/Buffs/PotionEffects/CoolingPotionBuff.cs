dd1e9f3ddc8cef60d70a11971436e7de27d5b967
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-12-24 22:59:11 -0800

    Cooling Potion/Buff

---
+++ Content/Buffs/PotionEffects/CoolingPotionBuff.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using TerraTemp.Utilities;
+
+namespace TerraTemp.Content.Buffs.PotionEffects {
+
+    public class CoolingPotionBuff : ModBuff {
+        private readonly float heatComfortabilityIncrease = 8f;
+
+        public override void SetDefaults() {
+            DisplayName.SetDefault("Cooled");
+            Description.SetDefault(TempUtilities.GetTerraTempTextValue("GlobalTooltip.IncreasedHeatComfortability", heatComfortabilityIncrease));
+        }
+
+        public override void Update(Player player, ref int buffIndex) {
+            player.GetModPlayer<TempPlayer>().comfortableHigh += heatComfortabilityIncrease;
+        }
+    }
+}
---
+++ Content/Items/Potions/CoolingPotion.cs
@@ -0,0 +1,33 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using TerraTemp.Content.Buffs.PotionEffects;
+using TerraTemp.Utilities;
+
+namespace TerraTemp.Content.Items.Potions {
+
+    public class CoolingPotion : ModItem {
+
+        //Carbon copy opposite of Warmth Potion
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Cooling Potion");
+            Tooltip.SetDefault(TempUtilities.GetTerraTempTextValue("GlobalTooltip.IncreasedHeatComfortability", 8f));
+        }
+
+        public override void SetDefaults() {
+            item.CloneDefaults(ItemID.WarmthPotion);
+            item.width = 32;
+            item.height = 32;
+            item.buffType = ModContent.BuffType<CoolingPotionBuff>();
+        }
+
+        public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.BottledWater);
+            recipe.AddIngredient(ItemID.FlarefinKoi);
+            recipe.AddIngredient(ItemID.Fireblossom);
+            recipe.AddTile(TileID.Bottles);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
