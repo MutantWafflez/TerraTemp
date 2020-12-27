f1731ee3107a4ffaec4f5620f812d52a5f198d69
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-12-27 15:41:32 -0800

    Fixed additional tooltips showing on accessories in vanity slots

--- Common/GlobalItems/VanillaItemChanges.cs
+++ Common/GlobalItems/VanillaItemChanges.cs
@@ -30,7 +30,7 @@ namespace TerraTemp.Common.GlobalItems {
 
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
             foreach (ItemChange change in TerraTemp.itemChanges) {
-                if ((item.type == change.AppliedItemID || change.AlternativeIDs.Contains(item.type)) && change.AdditionalTooltip != null) {
+                if ((item.type == change.AppliedItemID || change.AlternativeIDs.Contains(item.type)) && !item.social && change.AdditionalTooltip != null) {
                     TooltipLine newLine = new TooltipLine(mod, "TempAdditionalLine", change.AdditionalTooltip);
 
                     //These checks are so the new tooltips are placed properly and follow the normal formatting of vanilla tooltips.
