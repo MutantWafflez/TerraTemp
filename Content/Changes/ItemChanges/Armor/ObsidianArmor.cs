93bb9ee7d8250c5febcaeeee63d3708d8ad76b45
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-12-28 02:52:26 -0800

    Obsidian Armor Set Bonus

---
+++ Content/Changes/ItemChanges/Armor/ObsidianArmor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor {
+
+    public class ObsidianArmor : SetBonusChange {
+
+        public override List<int> HelmetPieceID => new List<int> {
+            ItemID.ObsidianHelm
+        };
+
+        public override int ChestPieceID => ItemID.ObsidianShirt;
+
+        public override int LegPieceID => ItemID.ObsidianPants;
+
+        public override string ArmorSetName => "ObsidianArmor";
+
+        public override string AdditionalSetBonusText => "Grants the effect of the Obsidian Rose";
+    }
+}
