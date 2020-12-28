af1b917489a44a7d6617c1f4ff1fec6e0495e426
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-12-28 02:42:48 -0800

    Hallowed Armor Set Bonus

---
+++ Content/Changes/ItemChanges/Armor/HallowedArmor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor {
+
+    public class HallowedArmor : SetBonusChange {
+
+        public override List<int> HelmetPieceID => new List<int> {
+            ItemID.HallowedHelmet,
+            ItemID.HallowedMask,
+            ItemID.HallowedHeadgear
+        };
+
+        public override int ChestPieceID => ItemID.HallowedPlateMail;
+
+        public override int LegPieceID => ItemID.HallowedGreaves;
+
+        public override string ArmorSetName => "HallowedArmor";
+
+        public override float TemperatureResistanceChange => 0.5f;
+    }
+}
