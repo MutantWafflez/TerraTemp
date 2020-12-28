230d529cb2e0b99740016e5071afa86cd7db75c0
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-12-28 14:37:00 -0800

    Wood Armor Temperature Effects

---
+++ Content/Changes/ItemChanges/Armor/WoodArmors.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor {
+
+    public class WoodHelmets : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int> {
+            ItemID.WoodHelmet,
+            ItemID.BorealWoodHelmet,
+            ItemID.PalmWoodHelmet,
+            ItemID.RichMahoganyHelmet,
+            ItemID.ShadewoodHelmet,
+            ItemID.EbonwoodHelmet,
+            ItemID.PearlwoodHelmet
+        };
+
+        public override float HeatComfortabilityChange => 0.5f;
+
+        public override float ColdComfortabilityChange => -0.5f;
+    }
+
+    public class WoodChestplates : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int> {
+            ItemID.WoodBreastplate,
+            ItemID.BorealWoodBreastplate,
+            ItemID.PalmWoodBreastplate,
+            ItemID.RichMahoganyBreastplate,
+            ItemID.ShadewoodBreastplate,
+            ItemID.EbonwoodBreastplate,
+            ItemID.PearlwoodBreastplate
+        };
+
+        public override float HeatComfortabilityChange => 1f;
+
+        public override float ColdComfortabilityChange => -1f;
+    }
+
+    public class WoodLeggings : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int> {
+            ItemID.WoodGreaves,
+            ItemID.BorealWoodGreaves,
+            ItemID.PalmWoodGreaves,
+            ItemID.RichMahoganyGreaves,
+            ItemID.ShadewoodGreaves,
+            ItemID.EbonwoodGreaves,
+            ItemID.PearlwoodGreaves
+        };
+
+        public override float HeatComfortabilityChange => 0.5f;
+
+        public override float ColdComfortabilityChange => -0.5f;
+    }
+}
