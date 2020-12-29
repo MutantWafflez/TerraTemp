addc433f62ba36c794888f5fafe58ee836e5764c
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-12-28 18:53:41 -0800

    Jungle Armor Effects & Set Bonus

---
+++ Content/Changes/ItemChanges/Armor/JungleArmor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using TerraTemp.Content.Changes.TempBiomes;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor {
+
+    public class JungleHat : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int> {
+            ItemID.JungleHat
+        };
+
+        public override float HeatComfortabilityChange => 1f;
+    }
+
+    public class JungleShirt : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int> {
+            ItemID.JungleShirt
+        };
+
+        public override float HeatComfortabilityChange => 3f;
+    }
+
+    public class JunglePants : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int> {
+            ItemID.JunglePants
+        };
+
+        public override float HeatComfortabilityChange => 1f;
+    }
+
+    public class JungleArmor : SetBonusChange {
+        private JungleClimate jungleClimateHumidity = new JungleClimate();
+
+        public override List<int> HelmetPieceID => new List<int> {
+            ItemID.JungleHat
+        };
+
+        public override int ChestPieceID => ItemID.JungleShirt;
+
+        public override int LegPieceID => ItemID.JunglePants;
+
+        public override string ArmorSetName => "JungleArmor";
+
+        public override string AdditionalSetBonusText => "Negates the humidity of the Jungle";
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            if (player.ZoneJungle) {
+                player.GetModPlayer<TempPlayer>().relativeHumidity -= jungleClimateHumidity.HumidityModification;
+            }
+        }
+    }
+}
