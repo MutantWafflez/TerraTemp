44637f0c5a72569b9b77678bc17a51b2b6dbe90e
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-12-28 18:34:48 -0800

    Crimson Humidity

--- Content/Changes/EvilClimates/CrimsonClimate.cs
+++ Content/Changes/EvilClimates/CrimsonClimate.cs
@@ -5,6 +5,8 @@ namespace TerraTemp.Content.Changes.EvilTempBiomes {
     public class CrimsonClimate : EvilClimate {
         public override float TemperatureModification => 7.5f;
 
+        public override float HumidityModification => 0.25f;
+
         public override float TemperatureResistanceModification => -0.1f;
 
         public override bool EvilZoneBool(Player player) => player.ZoneCrimson;
