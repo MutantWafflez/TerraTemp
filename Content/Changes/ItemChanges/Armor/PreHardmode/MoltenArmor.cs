c5354f03759d65c5d4dd5dd1d6f83b9bc689dd30
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-01 23:24:09 -0800

    Create MoltenArmor.cs

---
+++ Content/Changes/ItemChanges/Armor/PreHardmode/MoltenArmor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor.PreHardmode {
+
+    public class MoltenHelmet : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.MoltenHelmet
+        };
+
+        public override float DesiredTemperatureChange => 2f;
+    }
+
+    public class MoltenChestplate : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.MoltenBreastplate
+        };
+
+        public override float DesiredTemperatureChange => 4f;
+    }
+
+    public class MoltenLeggings : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.MoltenGreaves
+        };
+
+        public override float DesiredTemperatureChange => 2f;
+    }
+
+    public class MoltenArmor : SetBonusChange {
+
+        public override List<int> HelmetPieceID => new List<int>() {
+            ItemID.MoltenHelmet
+        };
+
+        public override int ChestPieceID => ItemID.MoltenBreastplate;
+
+        public override int LegPieceID => ItemID.MoltenGreaves;
+
+        public override string ArmorSetName => "MoltenArmor";
+
+        public override float ColdComfortabilityChange => -8f;
+
+        public override string AdditionalSetBonusText => base.AdditionalSetBonusText;
+    }
+}
