7f5f6b800c5e80b3d2a0e881f83c704074dfc869
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-02 03:20:47 -0800

    Campfires give additional heat

---
+++ Content/Changes/BuffChanges/Buffs/CampfireBuff.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.BuffChanges.Buffs {
+
+    public class CampfireBuff : BuffChange {
+        public override int AppliedBuffID => BuffID.Campfire;
+
+        public override float DesiredTemperatureChange => 2f;
+    }
+}
