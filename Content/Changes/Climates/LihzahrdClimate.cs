30af1d252fee20131fb0dd51b4135b96941eab72
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-01 22:45:49 -0800

    Update LihzahrdClimate.cs

--- Content/Changes/Climates/LihzahrdClimate.cs
+++ Content/Changes/Climates/LihzahrdClimate.cs
@@ -7,7 +7,7 @@ namespace TerraTemp.Content.Changes.Climates {
     public class LihzahrdClimate : Climate {
         public override float TemperatureModification => 10f;
 
-        public override float HumidityModification => 0.5f;
+        public override float HumidityModification => 0.80f;
 
         //Since vanilla doesn't have a bool for when the player is in the Lihzhard "Zone", we will do the check that vanilla does for it
         public override bool PlayerZoneBool(Player player) => Framing.GetTileSafely((int)(player.position.X + (float)(player.width / 2)) / 16, (int)(player.position.Y + (float)(player.height / 2)) / 16).wall == WallID.LihzahrdBrickUnsafe;
