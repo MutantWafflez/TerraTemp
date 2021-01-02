f4719ebbc0667cd33908349896275192a6ec5ad6
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-02 03:00:28 -0800

    Other Fire-Related Debuff Changes

---
+++ Content/Changes/BuffChanges/Debuffs/CursedFlameDebuff.cs
@@ -0,0 +1,12 @@
+﻿using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.BuffChanges.Debuffs {
+
+    public class CursedFlameDebuff : BuffChange {
+        public override int AppliedBuffID => BuffID.CursedInferno;
+
+        public override float DesiredTemperatureChange => 8f;
+
+        public override float TemperatureResistanceChange => -0.2f;
+    }
+}
---
+++ Content/Changes/BuffChanges/Debuffs/FrostburnDebuff.cs
@@ -0,0 +1,12 @@
+﻿using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.BuffChanges.Debuffs {
+
+    public class FrostburnDebuff : BuffChange {
+        public override int AppliedBuffID => BuffID.Frostburn;
+
+        public override float DesiredTemperatureChange => -8f;
+
+        public override float TemperatureResistanceChange => -0.2f;
+    }
+}
