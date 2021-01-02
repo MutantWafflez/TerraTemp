ac61b7e8f3f316c8022a8aeed030d6840f2ed5be
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-02 00:56:55 -0800

    Create SpectreArmor.cs

---
+++ Content/Changes/ItemChanges/Armor/Hardmode/SpectreArmor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
+
+    public class SpectreHelmet : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.SpectreHood,
+            ItemID.SpectreMask
+        };
+
+        public override float ColdComfortabilityChange => -4f;
+
+        public override float HeatComfortabilityChange => -2f;
+    }
+
+    public class SpectreChestplate : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.SpectreRobe
+        };
+
+        public override float ColdComfortabilityChange => -6f;
+
+        public override float HeatComfortabilityChange => -4f;
+    }
+
+    public class SpectreLeggings : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.SpectrePants
+        };
+
+        public override float ColdComfortabilityChange => -4f;
+
+        public override float HeatComfortabilityChange => -2f;
+    }
+
+    public class SpectreArmor : SetBonusChange {
+
+        public override List<int> HelmetPieceID => new List<int>() {
+            ItemID.SpectreHood,
+            ItemID.SpectreMask
+        };
+
+        public override int ChestPieceID => ItemID.SpectreRobe;
+
+        public override int LegPieceID => ItemID.SpectrePants;
+
+        public override string ArmorSetName => "SpectreArmor";
+
+        public override string AdditionalSetBonusText => "Gain up to 80% increased temperature change resistance as mana decreases";
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            if (player.statMana < player.statManaMax2) {
+                player.GetModPlayer<TempPlayer>().temperatureChangeResist += 0.8f * (1f - ((float)player.statMana / (float)player.statManaMax2));
+            }
+        }
+    }
+}
