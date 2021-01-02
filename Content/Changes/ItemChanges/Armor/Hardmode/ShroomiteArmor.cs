6d1744133ac41b7a4d7728665e287c92a113964c
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-02 00:36:17 -0800

    Create ShroomiteArmor.cs

---
+++ Content/Changes/ItemChanges/Armor/Hardmode/ShroomiteArmor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
+
+    public class ShroomiteHelmet : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.ShroomiteHelmet,
+            ItemID.ShroomiteHeadgear,
+            ItemID.ShroomiteMask
+        };
+
+        public override float TemperatureResistanceChange => 0.025f;
+    }
+
+    public class ShroomiteChestplate : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.ShroomiteBreastplate
+        };
+
+        public override float TemperatureResistanceChange => 0.05f;
+    }
+
+    public class ShroomiteLeggings : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.ShroomiteLeggings
+        };
+
+        public override float TemperatureResistanceChange => 0.025f;
+    }
+
+    public class ShroomiteArmor : SetBonusChange {
+
+        public override List<int> HelmetPieceID => new List<int>() {
+            ItemID.ShroomiteHelmet,
+            ItemID.ShroomiteHeadgear,
+            ItemID.ShroomiteMask
+        };
+
+        public override int ChestPieceID => ItemID.ShroomiteBreastplate;
+
+        public override int LegPieceID => ItemID.ShroomiteLeggings;
+
+        public override string ArmorSetName => "ShroomiteArmor";
+
+        public override string AdditionalSetBonusText => "80% increased temperature change resistance while in stealth";
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            player.GetModPlayer<TempPlayer>().temperatureChangeResist += 0.8f * (1f - player.stealth);
+        }
+    }
+}
