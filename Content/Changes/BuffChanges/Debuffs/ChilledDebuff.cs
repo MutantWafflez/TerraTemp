c0eaad086c4f13ee9d3a5de59e8ca38ae471c3a5
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-02 02:53:22 -0800

    Cold Debuff Temperature Changes

---
+++ Content/Changes/BuffChanges/Debuffs/ChilledDebuff.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.BuffChanges.Debuffs {
+
+    public class ChilledDebuff : BuffChange {
+        public override int AppliedBuffID => BuffID.Chilled;
+
+        public override float DesiredTemperatureChange => -5f;
+    }
+}
---
+++ Content/Changes/BuffChanges/Debuffs/FrozenDebuff.cs
@@ -0,0 +1,12 @@
+﻿using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.BuffChanges.Debuffs {
+
+    public class FrozenDebuff : BuffChange {
+        public override int AppliedBuffID => BuffID.Frozen;
+
+        public override float ColdComfortabilityChange => -10f;
+
+        public override float TemperatureResistanceChange => -0.33f;
+    }
+}
