88628f67d816475255d926f622e099e2c89b59ce
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-02 21:48:08 -0800

    Angler Armor Changes

---
+++ Content/Changes/ItemChanges/Armor/PreHardmode/AnglerArmor.cs
@@ -0,0 +1,49 @@
+﻿using Terraria.ID;
+using System.Collections.Generic;
+using Terraria;
+using TerraTemp.Utilities;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor.PreHardmode {
+
+    public class AnglerHelmet : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.AnglerHat
+        };
+
+        public override float HeatComfortabilityChange => 1f;
+    }
+
+    public class AnglerChestplate : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.AnglerVest
+        };
+
+        public override float HeatComfortabilityChange => 2f;
+    }
+
+    public class AnglerLeggings : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.AnglerPants
+        };
+
+        public override float HeatComfortabilityChange => 1f;
+    }
+
+    public class AnglerArmor : SetBonusChange {
+
+        public override List<int> HelmetPieceID => new List<int>() {
+            ItemID.AnglerHat
+        };
+
+        public override int ChestPieceID => ItemID.AnglerVest;
+
+        public override int LegPieceID => ItemID.AnglerPants;
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            player.GetTempPlayer().temperatureChangeResist += ((float)player.FishingLevel() / 100f) * 0.34f;
+        }
+    }
+}
