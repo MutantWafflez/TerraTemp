ab0478ca14808b105b09ea576241f0f4c10642ae
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-02 18:35:55 -0800

    Create MeteorArmor.cs

---
+++ Content/Changes/ItemChanges/Armor/PreHardmode/MeteorArmor.cs
@@ -0,0 +1,54 @@
+﻿using Terraria.ID;
+using System.Collections.Generic;
+using Terraria;
+using TerraTemp.Content.Changes.TempBiomes;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor.PreHardmode {
+
+    public class MeteorHelmet : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.MeteorHelmet
+        };
+
+        public override float ColdComfortabilityChange => -2f;
+    }
+
+    public class MeteorChestplate : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.MeteorSuit
+        };
+
+        public override float ColdComfortabilityChange => -3f;
+    }
+
+    public class MeteorLeggings : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.MeteorLeggings
+        };
+
+        public override float ColdComfortabilityChange => -2f;
+    }
+
+    public class MeteorArmor : SetBonusChange {
+        private readonly SpaceClimate spaceColdChange = new SpaceClimate();
+
+        public override List<int> HelmetPieceID => new List<int>() {
+            ItemID.MeteorHelmet
+        };
+
+        public override int ChestPieceID => ItemID.MeteorSuit;
+
+        public override int LegPieceID => ItemID.MeteorLeggings;
+
+        public override string AdditionalSetBonusText => "Negates the cold of Space";
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            if (player.ZoneSkyHeight) {
+                player.GetModPlayer<TempPlayer>().baseDesiredTemperature -= spaceColdChange.TemperatureModification;
+            }
+        }
+    }
+}
