f4011da8defc9978e3e7a88051b2dcc82ff513bc
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-02 17:51:50 -0800

    Create CrimsonArmor.cs

---
+++ Content/Changes/ItemChanges/Armor/PreHardmode/CrimsonArmor.cs
@@ -0,0 +1,56 @@
+﻿using Terraria.ID;
+using System.Collections.Generic;
+using Terraria;
+using TerraTemp.Content.Changes.EvilTempBiomes;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor.PreHardmode {
+
+    public class CrimsonHelmet : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.CrimsonHelmet
+        };
+
+        public override float TemperatureResistanceChange => 0.0125f;
+    }
+
+    public class CrimsonChestplate : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.CrimsonScalemail
+        };
+
+        public override float TemperatureResistanceChange => 0.025f;
+    }
+
+    public class CrimsonLeggings : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.CrimsonGreaves
+        };
+
+        public override float TemperatureResistanceChange => 0.0125f;
+    }
+
+    public class CrimsonArmor : SetBonusChange {
+        private readonly CrimsonClimate crimsonHumidityChange = new CrimsonClimate();
+
+        public override List<int> HelmetPieceID => new List<int>() {
+            ItemID.CrimsonHelmet
+        };
+
+        public override int ChestPieceID => ItemID.CrimsonScalemail;
+
+        public override int LegPieceID => ItemID.CrimsonGreaves;
+
+        public override string ArmorSetName => "CrimsonArmor";
+
+        public override string AdditionalSetBonusText => "Negates the humidity of the Crimson";
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            if (player.ZoneCrimson) {
+                player.GetModPlayer<TempPlayer>().relativeHumidity -= crimsonHumidityChange.HumidityModification;
+            }
+        }
+    }
+}
