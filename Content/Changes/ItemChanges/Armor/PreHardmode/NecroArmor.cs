10634c92077fa3a4963d7bd76edae1ebdd051302
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-02 19:48:14 -0800

    Create NecroArmor.cs

---
+++ Content/Changes/ItemChanges/Armor/PreHardmode/NecroArmor.cs
@@ -0,0 +1,48 @@
+﻿using Terraria.ID;
+using System.Collections.Generic;
+using Terraria;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor.PreHardmode {
+
+    public class NecroHelmet : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.NecroHelmet,
+            ItemID.AncientNecroHelmet
+        };
+
+        public override float CriticalTemperatureChange => 1f;
+    }
+
+    public class NecroChestplate : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.NecroBreastplate
+        };
+
+        public override float CriticalTemperatureChange => 1f;
+    }
+
+    public class NecroLeggings : ItemChange {
+
+        public override List<int> AppliedItemIDs => new List<int>() {
+            ItemID.NecroGreaves
+        };
+
+        public override float CriticalTemperatureChange => 1f;
+    }
+
+    public class NecroArmor : SetBonusChange {
+
+        public override List<int> HelmetPieceID => new List<int>() {
+            ItemID.NecroHelmet,
+            ItemID.AncientNecroHelmet
+        };
+
+        public override int ChestPieceID => ItemID.NecroBreastplate;
+
+        public override int LegPieceID => ItemID.NecroGreaves;
+
+        public override float CriticalTemperatureChange => 2f;
+    }
+}
