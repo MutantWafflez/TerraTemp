738ffd4029d711a611cd4ccd1b267f12f0d3ae9a
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-02 22:56:22 -0800

    Previous Commits Cleanup

--- Content/Changes/ItemChanges/Armor/PreHardmode/PumpkinArmor.cs
+++ Content/Changes/ItemChanges/Armor/PreHardmode/PumpkinArmor.cs
@@ -1,6 +1,6 @@
-﻿using Terraria.ID;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Terraria;
+using Terraria.ID;
 using TerraTemp.Utilities;
 
 namespace TerraTemp.Content.Changes.ItemChanges.Armor.PreHardmode {
