02e8167f13e5f5ee3876506e99012787b03dfc9d
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 04:13:22 -0800

    Stardust Armor Changes

--- Content/Changes/ItemChanges/Armor/Hardmode/StardustArmor.cs
+++ Content/Changes/ItemChanges/Armor/Hardmode/StardustArmor.cs
@@ -1,5 +1,7 @@
 ﻿using Terraria.ID;
 using System.Collections.Generic;
+using Terraria;
+using TerraTemp.Utilities;
 
 namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
 
@@ -8,6 +10,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override List<int> AppliedItemIDs => new List<int>() {
             ItemID.StardustHelmet
         };
+
+        public override float HeatComfortabilityChange => 1.5f;
+
+        public override float ColdComfortabilityChange => -1.5f;
     }
 
     public class StardustChestplate : ItemChange {
@@ -15,6 +21,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override List<int> AppliedItemIDs => new List<int>() {
             ItemID.StardustBreastplate
         };
+
+        public override float HeatComfortabilityChange => 1.5f;
+
+        public override float ColdComfortabilityChange => -1.5f;
     }
 
     public class StardustLeggings : ItemChange {
@@ -22,6 +32,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override List<int> AppliedItemIDs => new List<int>() {
             ItemID.StardustLeggings
         };
+
+        public override float HeatComfortabilityChange => 1.5f;
+
+        public override float ColdComfortabilityChange => -1.5f;
     }
 
     public class StardustArmor : SetBonusChange {
@@ -33,5 +47,11 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override int ChestPieceID => ItemID.StardustBreastplate;
 
         public override int LegPieceID => ItemID.StardustLeggings;
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            if (!Main.dayTime) {
+                player.GetTempPlayer().baseDesiredTemperature += 8f;
+            }
+        }
     }
 }
