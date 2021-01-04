239e6d52dc8b13811a6b345c5c915978c4db1655
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 03:54:30 -0800

    Vortex Armor Changes

--- Content/Changes/ItemChanges/Armor/Hardmode/VortexArmor.cs
+++ Content/Changes/ItemChanges/Armor/Hardmode/VortexArmor.cs
@@ -1,5 +1,7 @@
 ﻿using Terraria.ID;
 using System.Collections.Generic;
+using Terraria;
+using TerraTemp.Utilities;
 
 namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
 
@@ -8,6 +10,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override List<int> AppliedItemIDs => new List<int>() {
             ItemID.VortexHelmet
         };
+
+        public override float ColdComfortabilityChange => 2f;
+
+        public override float HeatComfortabilityChange => -2f;
     }
 
     public class VortexChestplate : ItemChange {
@@ -15,6 +21,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override List<int> AppliedItemIDs => new List<int>() {
             ItemID.VortexBreastplate
         };
+
+        public override float ColdComfortabilityChange => 3f;
+
+        public override float HeatComfortabilityChange => -3f;
     }
 
     public class VortexLeggings : ItemChange {
@@ -22,6 +32,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override List<int> AppliedItemIDs => new List<int>() {
             ItemID.VortexLeggings
         };
+
+        public override float ColdComfortabilityChange => 2f;
+
+        public override float HeatComfortabilityChange => -2f;
     }
 
     public class VortexArmor : SetBonusChange {
@@ -33,5 +47,9 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override int ChestPieceID => ItemID.VortexBreastplate;
 
         public override int LegPieceID => ItemID.VortexLeggings;
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            player.GetTempPlayer().temperatureChangeResist += 0.95f * (1f - player.stealth);
+        }
     }
 }
