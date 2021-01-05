ac873a78a1f2324e3ed9c976fd039a5be787e904
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 23:36:26 -0800

    Philosopher's Stone Changes

---
+++ Content/Changes/ItemChanges/Accessories/PhilosopherStone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class PhilosopherStone : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.PhilosophersStone,
+            ItemID.CharmofMyths
+        };
+
+        public override float TemperatureResistanceChange => 0.15f;
+    }
+}
