903a0fce2e92297c3d3dc35e22690293fcdbdb75
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 23:40:14 -0800

    Sun Stone Changes

---
+++ Content/Changes/ItemChanges/Accessories/SunStone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class SunStone : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.SunStone
+        };
+
+        public override float HeatComfortabilityChange => 3f;
+    }
+}
