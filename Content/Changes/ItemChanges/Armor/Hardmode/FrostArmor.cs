e082670f27dcc71ea4b523673e57778ad1b607ad
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 22:31:35 -0800

    Frost Armor Changes

--- Content/Changes/ItemChanges/Armor/Hardmode/FrostArmor.cs
+++ Content/Changes/ItemChanges/Armor/Hardmode/FrostArmor.cs
@@ -1,5 +1,7 @@
 ﻿using Terraria.ID;
 using System.Collections.Generic;
+using Terraria;
+using TerraTemp.Common.Players;
 
 namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
 
@@ -8,6 +10,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
             ItemID.FrostHelmet
         };
+
+        public override float ColdComfortabilityChange => -4f;
+
+        public override float HeatComfortabilityChange => -2f;
     }
 
     public class FrostChestplate : ItemChange {
@@ -15,6 +21,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
             ItemID.FrostBreastplate
         };
+
+        public override float ColdComfortabilityChange => -4f;
+
+        public override float HeatComfortabilityChange => -2f;
     }
 
     public class FrostLeggings : ItemChange {
@@ -22,6 +32,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
             ItemID.FrostLeggings
         };
+
+        public override float ColdComfortabilityChange => -4f;
+
+        public override float HeatComfortabilityChange => -2f;
     }
 
     public class FrostArmor : SetBonusChange {
@@ -33,5 +47,9 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override int ChestPieceID => ItemID.FrostBreastplate;
 
         public override int LegPieceID => ItemID.FrostLeggings;
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            player.GetModPlayer<AccessoryPlayer>().frostRune = true;
+        }
     }
 }
