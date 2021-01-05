88784aef03cdb6080666aa71984b58b1572f49da
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 23:56:41 -0800

    Ice Skates Changes

---
+++ Content/Changes/ItemChanges/Accessories/IceSkates.cs
@@ -0,0 +1,16 @@
+﻿using Terraria.ID;
+using System.Collections.Generic;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class IceSkates : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.IceSkates,
+            ItemID.ArcticDivingGear,
+            ItemID.FrostsparkBoots
+        };
+
+        public override float ColdComfortabilityChange => -3f;
+    }
+}
