24760260f2b0400a57d950d4dfe8aadc64548840
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 23:46:54 -0800

    Obsidian Skull Changes

---
+++ Content/Changes/ItemChanges/Accessories/ObsidianSkull.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class ObsidianSkull : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.ObsidianSkull,
+            ItemID.ObsidianWaterWalkingBoots,
+            ItemID.ObsidianShield,
+            ItemID.AnkhShield
+        };
+
+        public override float HeatComfortabilityChange => 2f;
+    }
+}
