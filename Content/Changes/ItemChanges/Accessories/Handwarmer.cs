3cbf57486eb1f6cc74bf1c26b9f9fd963d08d908
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 23:40:09 -0800

    Hand Warmer Changes

---
+++ Content/Changes/ItemChanges/Accessories/Handwarmer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class Handwarmer : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.HandWarmer
+        };
+
+        public override float ColdComfortabilityChange => -3f;
+    }
+}
