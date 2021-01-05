80a62dec47a5a0308b06be751a3a6760ca23a2a5
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 23:31:56 -0800

    Star Cloak Changes

---
+++ Content/Changes/ItemChanges/Accessories/StarCloak.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class StarCloak : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.StarCloak,
+            ItemID.StarVeil,
+            ItemID.BeeCloak
+        };
+
+        public override float ColdComfortabilityChange => -3f;
+    }
+}
