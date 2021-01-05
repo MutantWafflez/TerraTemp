58dfd32f86f1fdf97294af6b2bfd948553a3bb9a
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 23:53:08 -0800

    Flurry Boots Changes

---
+++ Content/Changes/ItemChanges/Accessories/FlurryBoots.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class FlurryBoots : ItemChange {
+        //TODO: When porting to 1.4, don't forget to add Dune Rider boots
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.FlurryBoots
+        };
+
+        public override float ColdComfortabilityChange => -2f;
+    }
+}
