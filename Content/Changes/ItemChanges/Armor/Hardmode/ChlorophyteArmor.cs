8b356462738614d5de29708659153fbdedc3bcb2
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 16:22:07 -0800

    Chlorophyte Armor Change

--- Content/Changes/ItemChanges/Armor/Hardmode/ChlorophyteArmor.cs
+++ Content/Changes/ItemChanges/Armor/Hardmode/ChlorophyteArmor.cs
@@ -1,5 +1,8 @@
 ﻿using Terraria.ID;
 using System.Collections.Generic;
+using Terraria;
+using TerraTemp.Utilities;
+using System.Linq;
 
 namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
 
@@ -10,6 +13,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
             ItemID.ChlorophyteMask,
             ItemID.ChlorophyteHeadgear
         };
+
+        public override float ColdComfortabilityChange => 1f;
+
+        public override float HeatComfortabilityChange => -1f;
     }
 
     public class ChlorophyteChestplate : ItemChange {
@@ -17,6 +24,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override List<int> AppliedItemIDs => new List<int>() {
             ItemID.ChlorophytePlateMail
         };
+
+        public override float ColdComfortabilityChange => 2f;
+
+        public override float HeatComfortabilityChange => -2f;
     }
 
     public class ChlorophyteLeggings : ItemChange {
@@ -24,6 +35,10 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override List<int> AppliedItemIDs => new List<int>() {
             ItemID.ChlorophyteGreaves
         };
+
+        public override float ColdComfortabilityChange => 1f;
+
+        public override float HeatComfortabilityChange => -1f;
     }
 
     public class ChlorophyteArmor : SetBonusChange {
@@ -37,5 +52,15 @@ namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
         public override int ChestPieceID => ItemID.ChlorophytePlateMail;
 
         public override int LegPieceID => ItemID.ChlorophyteGreaves;
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            List<int> listofCrits = new List<int>() {
+                player.magicCrit,
+                player.meleeCrit,
+                player.rangedCrit,
+                player.thrownCrit
+            };
+            player.GetTempPlayer().temperatureChangeResist += (float)listofCrits.Max() / 100f;
+        }
     }
 }
