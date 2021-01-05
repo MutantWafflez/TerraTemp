431487c3c10d03d65fd7cd75d4f333169e972ec1
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 23:52:55 -0800

    Magma Stone Changes

---
+++ Content/Changes/ItemChanges/Accessories/MagmaStone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class MagmaStone : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.MagmaStone,
+            ItemID.FireGauntlet
+        };
+
+        public override float DesiredTemperatureChange => 3f;
+    }
+}
