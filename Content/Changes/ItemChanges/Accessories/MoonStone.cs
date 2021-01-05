65e575039c03bcea6b866e71ca15e829a9c5e7ee
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-04 23:41:07 -0800

    Moon Stone Changes

---
+++ Content/Changes/ItemChanges/Accessories/MoonStone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories {
+
+    public class MoonStone : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.MoonStone
+        };
+
+        public override float ColdComfortabilityChange => -3f;
+    }
+}
