907305a8cf0a538db6a89cd9a1e325b3fc169c06
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-05 20:37:38 -0800

    Add Risk Badge Accessory

---
+++ Content/Items/Accessories/RiskBadge.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerraTemp.Content.Items.Accessories {
+
+    public class RiskBadge : TempEquipment {
+        //TODO: Get better sprite than what is currently here
+        public override float TemperatureResistanceChange => -10f;
+
+        public override void SetDefaults() {
+            item.width = 32;
+            item.height = 32;
+            item.accessory = true;
+            item.value = Item.sellPrice(gold: 1);
+            item.rare = ItemRarityID.LightRed;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual) {
+            player.endurance += 0.34f;
+        }
+
+        //Overriding this so the automatic tooltip system doesn't say "-1000% temperature change resistance"
+        //Check .lang file for actual translation
+        public override void ModifyTooltips(List<TooltipLine> tooltips) { }
+    }
+}
