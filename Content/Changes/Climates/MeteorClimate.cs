c9da0b89027e2ba6e941c4b45cda01bce1f83deb
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-05 18:48:17 -0800

    Add Meteor Climate

---
+++ Content/Changes/Climates/MeteorClimate.cs
@@ -0,0 +1,11 @@
+﻿using Terraria;
+
+namespace TerraTemp.Content.Changes.Climates {
+
+    public class MeteorClimate : Climate {
+
+        public override bool PlayerZoneBool(Player player) => player.ZoneMeteor;
+
+        public override float TemperatureModification => 17.5f;
+    }
+}
