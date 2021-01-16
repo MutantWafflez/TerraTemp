7cc74f8f4eabbecb6df9219c305b3d78019847d5
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-16 03:44:37 -0800

    Make underworld have 0% humidity no matter what

--- Content/Changes/Climates/UnderworldClimate.cs
+++ Content/Changes/Climates/UnderworldClimate.cs
@@ -5,6 +5,9 @@ namespace TerraTemp.Content.Changes.TempBiomes {
     public class UnderworldClimate : Climate {
         public override float TemperatureModification => 25f;
 
+        // It is absolutely, completely, 100% DRY in the Underworld.
+        public override float HumidityModification => -10f;
+
         public override bool PlayerZoneBool(Player player) => player.ZoneUnderworldHeight;
     }
 }
