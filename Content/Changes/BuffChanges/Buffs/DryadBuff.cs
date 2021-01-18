abfab48bf01c25c76be2fb40500a63eb0b7fc46e
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 19:19:43 -0800

    Dryad Ward now slightly increases global comfortability

---
+++ Content/Changes/BuffChanges/Buffs/DryadBuff.cs
@@ -0,0 +1,12 @@
+﻿using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.BuffChanges.Buffs {
+
+    public class DryadBuff : BuffChange {
+        public override int AppliedBuffID => BuffID.DryadsWard;
+
+        public override float ColdComfortabilityChange => -2f;
+
+        public override float HeatComfortabilityChange => 2f;
+    }
+}
