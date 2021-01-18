adc33f6f2f811ef1da520c182ba35339929de24f
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 20:24:38 -0800

    Buff Solar Eclipse Effects

--- Content/Changes/EventChanges/EclipseEvent.cs
+++ Content/Changes/EventChanges/EclipseEvent.cs
@@ -5,9 +5,9 @@ namespace TerraTemp.Content.Changes.EventChanges {
     public class EclipseEvent : EventChange {
         public override bool EventBoolean => Main.eclipse;
 
-        public override float DesiredTemperatureChange => 2.5f;
+        public override float DesiredTemperatureChange => 5f;
 
-        public override float HumidityChange => 0.25f;
+        public override float HumidityChange => 0.45f;
 
         public override bool ApplyEventEffects(Player player) => player.ZoneOverworldHeight;
     }
