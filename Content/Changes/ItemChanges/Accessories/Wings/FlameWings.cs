860c8cd6a1adcc9d2f40d15896595f4b4a55379f
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 20:28:22 -0800

    Flame/Frost Wings now give comfortability bonuses

---
+++ Content/Changes/ItemChanges/Accessories/Wings/FlameWings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories.Wings {
+
+    public class FlameWings : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.FlameWings
+        };
+
+        public override float HeatComfortabilityChange => 2f;
+    }
+}
---
+++ Content/Changes/ItemChanges/Accessories/Wings/FrostWings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Accessories.Wings {
+
+    public class FrostWings : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.FrozenWings
+        };
+
+        public override float ColdComfortabilityChange => -2f;
+    }
+}
