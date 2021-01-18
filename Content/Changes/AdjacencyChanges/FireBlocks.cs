0b1c1f5e283fd4510996f9affbcda54c44910fce
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 20:07:01 -0800

    Fire Block Adjacency now increases temperature

---
+++ Content/Changes/AdjacencyChanges/FireBlocks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.AdjacencyChanges {
+
+    public class FireBlocks : AdjacencyChange {
+
+        public override List<int> AppliedTileIDs => new List<int>() {
+            TileID.Hellstone,
+            TileID.HellstoneBrick,
+            TileID.Meteorite
+        };
+
+        public override float DesiredTemperatureChange => 3f;
+    }
+}
