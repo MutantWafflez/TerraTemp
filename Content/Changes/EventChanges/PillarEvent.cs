00fb7d11d6f9236d999b64f450d3b9eedf4e663c
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 20:37:08 -0800

    All Pillars now have unique psuedo-climates

---
+++ Content/Changes/EventChanges/PillarEvent.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+
+namespace TerraTemp.Content.Changes.EventChanges {
+
+    public class SolarPillar : EventChange {
+        public override bool EventBoolean => true;
+
+        public override float DesiredTemperatureChange => 6f;
+
+        public override bool ApplyEventEffects(Player player) => player.ZoneTowerSolar;
+    }
+
+    public class VortexPillar : EventChange {
+        public override bool EventBoolean => true;
+
+        public override float DesiredTemperatureChange => -6f;
+
+        public override bool ApplyEventEffects(Player player) => player.ZoneTowerVortex;
+    }
+
+    public class NebulaPillar : EventChange {
+        public override bool EventBoolean => true;
+
+        public override float HumidityChange => 0.75f;
+
+        public override bool ApplyEventEffects(Player player) => player.ZoneTowerNebula;
+    }
+
+    public class StardustPillar : EventChange {
+        public override bool EventBoolean => true;
+
+        public override float TemperatureResistanceChange => -0.75f;
+
+        public override bool ApplyEventEffects(Player player) => player.ZoneTowerStardust;
+    }
+}
