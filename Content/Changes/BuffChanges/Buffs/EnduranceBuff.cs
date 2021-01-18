bbf174a15e70843ec383be545835554c16d85346
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 19:55:28 -0800

    Endurance Buff/Potion now gives temp resistance

---
+++ Content/Changes/BuffChanges/Buffs/EnduranceBuff.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.BuffChanges.Buffs {
+
+    public class EnduranceBuff : BuffChange {
+        public override int AppliedBuffID => BuffID.Endurance;
+
+        public override float TemperatureResistanceChange => 0.15f;
+    }
+}
---
+++ Content/Changes/ItemChanges/Potions/EndurancePotion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+using TerraTemp.Content.Changes.BuffChanges.Buffs;
+using TerraTemp.Utilities;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Potions {
+
+    public class EndurancePotion : ItemChange {
+        private readonly EnduranceBuff enduranceBuff = new EnduranceBuff();
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.EndurancePotion
+        };
+
+        public override string AdditionalTooltip => TempUtilities.GetTerraTempTextValue("GlobalTooltip.IncreasedTempResistance", enduranceBuff.TemperatureResistanceChange * 100f);
+    }
+}
