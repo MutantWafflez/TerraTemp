9034dab7a264f75314ccfa6e7f208b5678c9109d
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 19:16:28 -0800

    Hypothermia/Heat Stroke Nerf/Buff

--- Content/Buffs/TempEffects/Hypothermia.cs
+++ Content/Buffs/TempEffects/Hypothermia.cs
@@ -12,8 +12,9 @@ namespace TerraTemp.Content.Buffs.TempEffects {
         }
 
         public override void Update(Player player, ref int buffIndex) {
-            player.statLifeMax2 = (int)(player.statLifeMax2 * 0.67f);
+            player.statLifeMax2 = (int)(player.statLifeMax2 * 0.75f);
             player.statDefense = (int)(player.statDefense * 0.67f);
+            player.allDamageMult -= 0.25f;
             player.meleeSpeed *= 0.75f;
             player.moveSpeed *= 0.75f;
             player.chilled = true;
