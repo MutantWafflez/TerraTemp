edd8ee255054d2a580a989f043e02d770960a697
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 18:23:09 -0800

    Add Mushroom Biome Climate

---
+++ Content/Changes/Climates/MushroomClimate.cs
@@ -0,0 +1,13 @@
+﻿using Terraria;
+
+namespace TerraTemp.Content.Changes.Climates {
+
+    public class MushroomClimate : Climate {
+
+        public override bool PlayerZoneBool(Player player) => player.ZoneGlowshroom;
+
+        public override float TemperatureModification => 7.5f;
+
+        public override float HumidityModification => 0.8f;
+    }
+}
