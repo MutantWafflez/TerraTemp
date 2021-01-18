e19aad8e2cffeb1ae319636a8fa47deb59ea26fe
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 19:07:29 -0800

    Desert is now more dry

--- Content/Changes/Climates/DesertClimate.cs
+++ Content/Changes/Climates/DesertClimate.cs
@@ -5,6 +5,8 @@ namespace TerraTemp.Content.Changes.TempBiomes {
     public class DesertClimate : Climate {
         public override float TemperatureModification => 10f;
 
+        public override float HumidityModification => -0.34f;
+
         public override bool PlayerZoneBool(Player player) => player.ZoneDesert;
     }
 }
