a277b9b5951f12a6959a241a3a074246b8b0a7c1
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 19:15:05 -0800

    Jungle is slightly less hot

--- Content/Changes/Climates/JungleClimate.cs
+++ Content/Changes/Climates/JungleClimate.cs
@@ -3,7 +3,7 @@
 namespace TerraTemp.Content.Changes.TempBiomes {
 
     public class JungleClimate : Climate {
-        public override float TemperatureModification => 12.5f;
+        public override float TemperatureModification => 10.5f;
 
         public override float HumidityModification => 0.34f;
 
