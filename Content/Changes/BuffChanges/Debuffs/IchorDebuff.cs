e127bb0405e0086ed24227d50d5066e630dd94b0
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 19:12:13 -0800

    Ichor now decreases temp resistance & increase climate extremity

---
+++ Content/Changes/BuffChanges/Debuffs/IchorDebuff.cs
@@ -0,0 +1,12 @@
+﻿using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.BuffChanges.Debuffs {
+
+    public class IchorDebuff : BuffChange {
+        public override int AppliedBuffID => BuffID.Ichor;
+
+        public override float TemperatureResistanceChange => -0.67f;
+
+        public override float ClimateExtremityChange => 0.25f;
+    }
+}
