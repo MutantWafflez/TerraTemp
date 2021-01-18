586def6655ea43f0c284fa42cf930d5a5f398238
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 19:05:45 -0800

    Campfires now give heat based on adjacency

---
+++ Content/Changes/AdjacencyChanges/Campfires.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.AdjacencyChanges {
+
+    public class Campfires : AdjacencyChange {
+
+        public override List<int> AppliedTileIDs => new List<int>() {
+            TileID.Campfire,
+            TileID.Fireplace
+        };
+
+        public override float DesiredTemperatureChange => 3f;
+    }
+}
