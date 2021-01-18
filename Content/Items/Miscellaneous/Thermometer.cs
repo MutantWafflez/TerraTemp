0c291ca95c8d344379831be58d042fad5390c89b
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-17 18:07:18 -0800

    Move Thermometer Location

--- Content/Items/Miscellaneous/Thermometer.cs
+++ Content/Items/Miscellaneous/Thermometer.cs
@@ -6,7 +6,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using TerraTemp.Utilities;
 
-namespace TerraTemp.Content.Items {
+namespace TerraTemp.Content.Items.Miscellaneous {
 
     public class Thermometer : ModItem {
 
