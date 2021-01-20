acb5891d8622530ffaba7d1bc98755526ed5b529
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-20 00:24:38 -0800

    Obligatory Solution Cleanup

--- Content/ModChanges/EventChanges/VariousWeathers/ColdFront.cs
+++ Content/ModChanges/EventChanges/VariousWeathers/ColdFront.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Terraria;
+﻿using Terraria;
 using TerraTemp.Custom.Attributes;
 using TerraTemp.Custom.Classes.ReflectionMod;
 
--- Content/ModChanges/EventChanges/VariousWeathers/HeatWave.cs
+++ Content/ModChanges/EventChanges/VariousWeathers/HeatWave.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Terraria;
+﻿using Terraria;
 using TerraTemp.Custom.Attributes;
 using TerraTemp.Custom.Classes.ReflectionMod;
 
