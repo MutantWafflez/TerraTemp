cdc24e48b998fe60d4652f363d333a95e84bf4a1
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-21 02:10:17 -0800

    Obligatory Solution Cleanup

--- Custom/TempUtilities.cs
+++ Custom/TempUtilities.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -6,7 +7,6 @@ using System.Text.RegularExpressions;
 using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
-using Microsoft.Xna.Framework;
 using TerraTemp.Custom.Attributes;
 using TerraTemp.Custom.Interfaces;
 
