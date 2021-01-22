9136159694b718abea44f74ec495e9dccd66fc2c
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-22 02:56:56 -0800

    Adjacency to Furnaces now give small heat bonuses

---
+++ Content/Changes/AdjacencyChanges/Furnaces.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+
+namespace TerraTemp.Content.Changes.AdjacencyChanges {
+
+    public class Furnaces : AdjacencyChange {
+
+        public override List<int> AppliedTileIDs => new List<int>() {
+            TileID.Furnaces,
+            TileID.Hellforge,
+            TileID.AdamantiteForge,
+            TileID.LihzahrdFurnace,
+            TileID.GlassKiln
+        };
+
+        public override float GetDesiredTemperatureChange(Player player) => 2f;
+    }
+}
