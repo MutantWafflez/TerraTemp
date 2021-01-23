f74f9766b683bc44c63c694a4871229e50f3bf69
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-23 04:06:50 -0800

    Calamity Compat. pt. 1

---
+++ Content/ModChanges/EventChanges/Calamity/CalamityTemperature.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using TerraTemp.Custom.Attributes;
+using TerraTemp.Custom.Classes.ReflectionMod;
+
+namespace TerraTemp.Content.ModChanges.EventChanges.Calamity {
+
+    [PertainedMod(typeof(CalamityMod))]
+    public class CalamityTemperature : ModEvent {
+
+        public CalamityTemperature(ReflectionMod reflectionModInstance) : base(reflectionModInstance) { }
+
+        public override bool ApplyEventEffects(Player player) {
+            //TODO: Fix Cold Resistance protecting against "Chilled"
+            (reflectionModInstance as CalamityMod).RemoveDeathModeTemperatureSystem(player);
+            return base.ApplyEventEffects(player);
+        }
+    }
+}
---
+++ Custom/Classes/ReflectionMod/CalamityMod.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using TerraTemp.Custom.Attributes;
+
+namespace TerraTemp.Custom.Classes.ReflectionMod {
+
+    [InternalModName("CalamityMod")]
+    public class CalamityMod : ReflectionMod {
+
+        public CalamityMod(Mod instance) : base(instance) { }
+
+        /// <summary>
+        /// Makes the given player Cold and Heat immune since TerraTemp already has that covered.
+        /// </summary>
+        /// <param name="player"> The player to remove the temperature system for. </param>
+        public void RemoveDeathModeTemperatureSystem(Player player) {
+            ModInstance.Call("MakeColdImmune", player);
+            ModInstance.Call("MakeHeatImmune", player);
+        }
+    }
+}
