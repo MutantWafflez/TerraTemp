2b17fc0f2097614d0a2c672447594b08babda7b4
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-01-26 21:02:54 -0800

    Wind Speeds are forced to zero Day 1

--- TempWorld.cs
+++ TempWorld.cs
@@ -6,6 +6,14 @@ namespace TerraTemp {
 
     public class TempWorld : ModWorld {
 
+        //Wind speeds should be completely reset on world gen. Don't want potentially hurricane level winds on day 1!
+        public override void PostWorldGen() {
+            Main.windSpeedSet = 0f;
+            Main.windSpeed = 0f;
+            Main.windSpeedSpeed = 0f;
+            Main.windSpeedTemp = 0f;
+        }
+
         #region I/O
 
         public override TagCompound Save() {
