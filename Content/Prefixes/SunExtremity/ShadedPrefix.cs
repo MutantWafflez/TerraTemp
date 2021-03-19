f38acdc0d0112e97f4fcb3da3bbac480f5c91759
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-03-19 14:06:42 -0700

    Sun Extremity Prefix

---
+++ Content/Prefixes/SunExtremity/ShadedPrefix.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+
+namespace TerraTemp.Content.Prefixes.SunExtremity {
+
+    public class ShadedPrefix : TemperatureAccessoryPrefix {
+
+        public override void SetDefaults() {
+            DisplayName.SetDefault("Shaded");
+        }
+
+        public override void ModifyValue(ref float valueMult) {
+            valueMult += 0.125f;
+        }
+
+        public override float GetSunExtremityChange(Player player) => -0.1f;
+    }
+}
