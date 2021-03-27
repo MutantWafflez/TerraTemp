7eb5a56964a6810578b79b6075f704f1ca57a3e6
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-03-27 08:10:29 -0700

    Tome Data

--- Content/Items/Tomes/FlameTome.cs
+++ Content/Items/Tomes/FlameTome.cs
@@ -1,7 +1,49 @@
-﻿using Terraria.ModLoader;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace TerraTemp.Content.Items.Tomes {
 
     public class FlameTome : ModItem {
+        public int tomeLevel;
+
+        public override bool CloneNewInstances => true;
+
+        public FlameTome() {
+            tomeLevel = 1;
+        }
+
+        public override ModItem Clone(Item item) {
+            FlameTome clonedItem = (FlameTome)base.Clone(item);
+            clonedItem.tomeLevel = tomeLevel;
+            return clonedItem;
+        }
+
+        public override void SetDefaults() {
+            item.rare = ItemRarityID.Green;
+            item.value = Item.buyPrice(silver: 20);
+            item.material = true;
+            item.maxStack = 1;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips) {
+            TooltipLine tomeLevelLine = new TooltipLine(mod, "TomeLevel", "Level " + tomeLevel) {
+                overrideColor = Color.Orange
+            };
+            tooltips.Add(tomeLevelLine);
+        }
+
+        public override TagCompound Save() {
+            return new TagCompound {
+                {"Level", tomeLevel}
+            };
+        }
+
+        public override void Load(TagCompound tag) {
+            tomeLevel = tag.GetInt("Level");
+        }
     }
 }
--- Content/Items/Tomes/FrostTome.cs
+++ Content/Items/Tomes/FrostTome.cs
@@ -1,7 +1,49 @@
-﻿using Terraria.ModLoader;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace TerraTemp.Content.Items.Tomes {
 
     public class FrostTome : ModItem {
+        public int tomeLevel;
+
+        public override bool CloneNewInstances => true;
+
+        public FrostTome() {
+            tomeLevel = 1;
+        }
+
+        public override ModItem Clone(Item item) {
+            FrostTome clonedItem = (FrostTome)base.Clone(item);
+            clonedItem.tomeLevel = tomeLevel;
+            return clonedItem;
+        }
+
+        public override void SetDefaults() {
+            item.rare = ItemRarityID.Green;
+            item.value = Item.buyPrice(silver: 20);
+            item.material = true;
+            item.maxStack = 1;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips) {
+            TooltipLine tomeLevelLine = new TooltipLine(mod, "TomeLevel", "Level " + tomeLevel) {
+                overrideColor = Color.Cyan
+            };
+            tooltips.Add(tomeLevelLine);
+        }
+
+        public override TagCompound Save() {
+            return new TagCompound {
+                {"Level", tomeLevel}
+            };
+        }
+
+        public override void Load(TagCompound tag) {
+            tomeLevel = tag.GetInt("Level");
+        }
     }
 }
