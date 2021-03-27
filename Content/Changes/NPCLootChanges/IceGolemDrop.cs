2eeab3e1a34bf7ebcaed898cdfe0d2ed121a3a77
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-03-27 08:44:36 -0700

    Tome Drop Customization

---
+++ Content/Changes/NPCLootChanges/IceGolemDrop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerraTemp.Content.Items.Tomes;
+using TerraTemp.Custom.Structs;
+
+namespace TerraTemp.Content.Changes.NPCLootChanges {
+
+    public class IceGolemDrop : NPCLootChange {
+        public override int AppliedNPCID => NPCID.IceGolem;
+
+        public override List<ItemDrop> ItemsToDrop => new List<ItemDrop>() {
+            new ItemDrop(ModContent.ItemType<FrostTome>(), new Tuple<int, int>(1, 1), () => Main.rand.Next(0, 9) == 0, delegate(int itemIndex) {
+                ((FrostTome)Main.item[itemIndex].modItem).tomeLevel = 2;
+            })
+        };
+    }
+}
---
+++ Content/Changes/NPCLootChanges/RedDevilDrop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerraTemp.Content.Items.Tomes;
+using TerraTemp.Custom.Structs;
+
+namespace TerraTemp.Content.Changes.NPCLootChanges {
+
+    public class RedDevilDrop : NPCLootChange {
+        public override int AppliedNPCID => NPCID.RedDevil;
+
+        public override List<ItemDrop> ItemsToDrop => new List<ItemDrop>() {
+            new ItemDrop(ModContent.ItemType<FlameTome>(), new Tuple<int, int>(1, 1), () => Main.rand.Next(0, 9) == 0, delegate(int itemIndex) {
+                ((FlameTome)Main.item[itemIndex].modItem).tomeLevel = 2;
+            })
+        };
+    }
+}
