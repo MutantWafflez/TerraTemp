8e2d15e7870dc6e74a70f30b06c479ddcbd3473d
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-04-01 21:27:48 -0700

    Fixed Frozen Debuff Accidentally Buffing Player

--- Content/Changes/BuffChanges/Debuffs/FrozenDebuff.cs
+++ Content/Changes/BuffChanges/Debuffs/FrozenDebuff.cs
@@ -6,7 +6,7 @@ namespace TerraTemp.Content.Changes.BuffChanges.Debuffs {
     public class FrozenDebuff : BuffChange {
         public override int AppliedBuffID => BuffID.Frozen;
 
-        public override float GetColdComfortabilityChange(Player player) => -10f;
+        public override float GetDesiredTemperatureChange(Player player) => -5f;
 
         public override float GetTemperatureResistanceChange(Player player) => -0.33f;
     }
