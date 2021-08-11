fa3f0cbc1e53b91cfb6d75619b7ed2038d04a6c2
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-08-11 01:02:03 -0700

    Add Crystal Assassin Armor Bonuses

---
+++ Content/Changes/ItemChanges/Armor/Hardmode/CrystalAssassinArmor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using TerraTemp.Custom;
+
+namespace TerraTemp.Content.Changes.ItemChanges.Armor.Hardmode {
+
+    public class CrystalAssassinHelmet : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.CrystalNinjaHelmet
+        };
+
+        public override float GetColdComfortabilityChange(Player player) => -2f;
+    }
+
+    public class CrystalAssassinChestplate : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.CrystalNinjaChestplate
+        };
+
+        public override float GetColdComfortabilityChange(Player player) => -3f;
+    }
+
+    public class CrystalAssassinLeggings : ItemChange {
+
+        public override HashSet<int> AppliedItemIDs => new HashSet<int>() {
+            ItemID.CrystalNinjaLeggings
+        };
+
+        public override float GetColdComfortabilityChange(Player player) => -2f;
+    }
+
+    public class CrystalAssassinArmor : SetBonusChange {
+
+        public override HashSet<int> HelmetPieceID => new HashSet<int>() {
+            ItemID.CrystalNinjaHelmet
+        };
+
+        public override int ChestPieceID => ItemID.CrystalNinjaChestplate;
+
+        public override int LegPieceID => ItemID.CrystalNinjaLeggings;
+
+        public override void AdditionalSetBonusEffect(Player player) {
+            if (!Main.dayTime) {
+                player.GetTempPlayer().temperatureChangeResist += 0.34f;
+            }
+        }
+    }
+}
