400eda86a27478f20ebd14aaafcac303c4493476
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2023-11-05 16:47:39 -0800

    New beginnings

---
+++ Common/Players/TemperaturePlayer.cs
@@ -0,0 +1,37 @@
+﻿namespace TerraTemp.Common.Players {
+    public class TemperaturePlayer : ModPlayer {
+        private const double DefaultBodyTemperature = 36.5d;
+
+        /// <summary>
+        /// The player's current temperature.
+        /// </summary>
+        public double CurrentBodyTemperature {
+            get;
+            private set;
+        }
+
+        public double AmbientTemperature {
+            get;
+            private set;
+        }
+
+        public double AmbientHumidity {
+            get;
+            private set;
+        }
+
+        public override void ResetEffects() {
+            AmbientTemperature = DefaultBodyTemperature;
+            AmbientHumidity = 0;
+        }
+
+        public override void UpdateDead() {
+            CurrentBodyTemperature = DefaultBodyTemperature;
+        }
+
+        public override void PostUpdate() {
+            // TODO: Do numbers testing
+            CurrentBodyTemperature += (AmbientTemperature - CurrentBodyTemperature) / 400f;
+        }
+    }
+}
---
+++ Content/AccessorySlots/CapeSlot.cs
@@ -0,0 +1,9 @@
+﻿namespace TerraTemp.Content.AccessorySlots {
+    public class CapeSlot : ModAccessorySlot {
+        public override bool DrawVanitySlot => false;
+
+        public override bool DrawDyeSlot => false;
+
+        public override bool CanAcceptItem(Item checkItem, AccessorySlotType context) => true;
+    }
+}
